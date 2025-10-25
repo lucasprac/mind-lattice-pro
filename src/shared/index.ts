9fed2aa09f94151a6b4ff0aaeb65307a6d11e9bb
lucasprac <53185497+lucasprac@users.noreply.github.com>
2025-10-25 12:05:33 -0300

    feat(shared): criar barrel exports para melhor organização de imports

---
+++ src/shared/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Barrel exports para módulo shared
+ * Centraliza todos os exports públicos do módulo
+ */
+
+// Constants
+export * from './constants/app.constants';
+
+// Schemas
+export * from './schemas/validation.schemas';
+
+// Services
+export * from './services/base.service';
+
+// Utils
+export * from './utils/common.utils';
